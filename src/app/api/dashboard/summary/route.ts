01ffeb053dadb91c89de282a1bbe62fd7a64384d
Shree2124 <shreealasande@gmail.com>
2026-01-15 17:15:51 +0530

    feat: Admin: summary api

--- src/app/api/dashboard/summary/route.ts
+++ src/app/api/dashboard/summary/route.ts
@@ -3,32 +3,45 @@ import { supabase } from "@/lib/dbConnect";
 import { auditProject } from "@/services/geminiService";
 import { OfficialRecord } from "@/types/types";
 
-const reportCount = async ()=>{
-    const { count, error } = await supabase
+const reportCount = async () => {
+  const { count, error } = await supabase
+    .from("citizen_reports")
+    .select("*", { count: "exact", head: true });
+
+  if (error) throw new Error("error while fetching citizen reports");
+
+  return count;
+};
+
+const riskCount = async (riskLevel: string) => {
+  const { count, error } = await supabase
     .from("citizen_reports")
     .select("*", { count: "exact", head: true })
+    .eq("risk_level", riskLevel);
 
-    if(error) throw new Error("error while fetching citizen reports")
+  if (error) throw error;
+  return count;
+};
 
-    return count
-}
-
-const highRiskCount = async ()=>{
+const countTotalProjects = async()=>{
     const { count, error } = await supabase
-    .from("citizen_reports")
-    .select("*", { count: "exact", head: true })
-    .eq("","")
+    .from("government_projects")
+    .select("*", { count: "exact", head: true });
 
-    if(error) throw new Error("error while fetching citizen reports")
+  if (error) throw new Error("error while fetching projects reports");
 
-    return count
+  return count;
 }
 
 export async function POST(req: NextRequest) {
   try {
-    const { count: totalReports, error: error1 } = await supabase
-      .from("citizen_reports")
-      .select("*", { count: "exact", head: true });
+    const totalCount = await reportCount();
+    const highRiskCount = await riskCount("High");
+    const mediumRiskCount = await riskCount("Medium");
+    const lowRiskCount = await riskCount("Low");
+    const totalProjects = await countTotalProjects()
+
+    return NextResponse.json({totalProjects,toatalReportCount: totalCount, highRiskCount, mediumRiskCount, lowRiskCount})
   } catch (error) {
     console.error("DASHBOARD/SUMMARY API Error:", error);
     return NextResponse.json(
